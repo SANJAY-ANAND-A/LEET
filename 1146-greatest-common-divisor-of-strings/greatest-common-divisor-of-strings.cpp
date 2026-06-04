e34d069161b712b37e842c3ec241d5c4fae8f589
Sanjay Anand A <anandsanjay2006@gmail.com>
2026-06-04 23:25:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (32.49%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,9 @@
+#include<numeric>
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if(str1+str2 != str2+str1) return "";
+        int gcd = std::gcd(str1.size(),str2.length());
+        return str1.substr(0,gcd);
+    }
+};
