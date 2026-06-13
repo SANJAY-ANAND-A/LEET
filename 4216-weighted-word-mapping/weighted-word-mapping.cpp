a5da3ce1d2ff8a56d1bf5921ec2d834f02c7ea8b
Sanjay Anand A <anandsanjay2006@gmail.com>
2026-06-14 00:58:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (95.02%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.cpp
+++ 4216-weighted-word-mapping/weighted-word-mapping.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public: 
     string mapWordWeights(vector<string>& words, vector<int>& weights) {
-         string result;
+        string result;
         for (auto &word : words) {
             int sum = 0;
             for (char c : word) {
