ad979e4aa03b60ea69678ca1f84ee0a9723ed5c7
Sanjay Anand A <anandsanjay2006@gmail.com>
2026-09-14 11:00:35 +0530

    Time: 6 ms (7.72%) | Memory: 63 MB (48.89%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,23 +1,31 @@
 class Solution {
-    private:
-    int Carea(int st, int end , int i , int j)
-    {
-        int a= (min(st,end)*(j-i));
-        return a;
-    }
 public:
+long long getArea(int l ,int i, int j)
+{
+    long long b = (long long)j-i;
+    return l*b;
+}
     int maxArea(vector<int>& height) {
-        int area =0;
-        int i = 0;
+        
+        int maxht=0;
+        int tarea,area = 0;
+        int i=0;
         int j = height.size()-1;
         while(i<j)
         {
-           int tarea = Carea(height[i],height[j],i,j);
-           area = max(area , tarea);
+        int h = min(height[i],height[j]);
+         tarea = getArea(h,i,j);
+        if(tarea > area)
+        {
+            area = tarea;
+        }
         if(height[i]<height[j])
         {
             i++;
-        }else{j--;}
+        }
+        else{
+            j--;
+        }
         }
         return area;
     }
